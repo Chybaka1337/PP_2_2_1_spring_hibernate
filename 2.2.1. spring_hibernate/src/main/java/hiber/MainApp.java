7f0dce401087381d163aad4d1fefdb943d196260
Andrey Zommer <zommer.02@mail.ru>
2024-08-18 22:09:34 +0300

    fix

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -12,8 +12,8 @@ import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
       CarService carService = context.getBean(CarService.class);
@@ -22,10 +22,11 @@ public class MainApp {
       Car carB = new Car("model_b", 2);
       Car carC = new Car("model_c", 3);
       Car carD = new Car("model_d", 4);
-      carService.addCar(carA)
-      carService.addCar(carB)
-      carService.addCar(carC)
-      carService.addCar(carD)
+      carService.addCar(carA);
+      carService.addCar(carB);
+      carService.addCar(carC);
+      carService.addCar(carD);
+
       userService.add(new User("User1", "Lastname1", "user1@mail.ru", carA));
       userService.add(new User("User2", "Lastname2", "user2@mail.ru", carB));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru", carC));
@@ -33,15 +34,15 @@ public class MainApp {
 
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
          System.out.println("Car = " + user.getCar());
          System.out.println();
       }
 
-      List<User> list = List.Of(userService.getUser(),
+      List<User> list = List.of(
               userService.getUser("model_a", 1),
               userService.getUser("model_b", 2),
               userService.getUser("model_c", 3),
